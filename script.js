36dcae82b574069a6a69811d93faf4e39305a306
mikhail2152 <mikeigumnov1@icloud.com>
2025-02-12 15:55:34 +0000

    Started

--- script.js
+++ script.js
@@ -1,48 +1,11 @@
-/* Nim Trainer by [your name]
- * based on this flowchart:
- * https://lucid.app/lucidchart/2018baaf-4c26-4a76-a0d5-93c97f444425/view
- */
+ function main(){
+   var count= 0;
+   while(count < 21){
+    var turn = prompt ("Next Count");
+    if (Turn ==1 || Turn == 2|| Turn == 3){
+        
+    }
 
-/* Global Variables */
+   }
 
-
-/** 
- * main  
- * Handles new Nim games with gametype choice simple or trainer and a play again option. 
- * @param none 
- * @return none
- */
-/* Main */
-function main(){
-
-}
-
-/** 
- * playNim 
- * plays a game with user first and computer second. Winner declared in an alert box. 
- * @param none 
- * @return none
- */
-function playNim(){
-
-}
-
-/** 
- * userTurn  
- * User enters a turn. Validation against cheating handled by recursion.
- * @param none 
- * @return none
- */
-function userTurn(){
-
-}
-
-/** 
- * cpuTurn 
- * Generate computer's turn without losing on purpose.  Different turns if trainer or simple.  
- * @param none 
- * @return none
- */
-function cpuTurn(){
-
-}
+ }
